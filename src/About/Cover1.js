38dd184905016601bc70635037a3599fd7b9e178
itsaimal <75414938+itsaimal@users.noreply.github.com>
2021-01-27 14:56:07 +0500

    Update Cover1.js

--- src/About/Cover1.js
+++ src/About/Cover1.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Cover1() {
     return (
         <div className="md:flex justify-center mt-10">
-        <img className="max-w-2xl" src="https://fleming.app/img/landing/svg/abstractions/rocket-abstraction.svg"/>
+        <img className="max-w-2xl" src="https://fleming.app/img/landing/svg/abstractions/rocket-abstraction.svg" alt="pic"/>
         <div className="max-w-lg ml-36 mr-24 mt-20">
             <h1 className="text-4xl font-semibold mb-4 text-blue-600">About</h1>
             <p className="text-xl font-normal">We - Ltd. Medical Score team believe in <strong className="text-blue-900"> available</strong>, modern, and <strong className="text-blue-900">affordable</strong></p>

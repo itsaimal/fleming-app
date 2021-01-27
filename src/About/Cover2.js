7c944a86a42bab60cf812d29c4016f1f0b9ee5d1
itsaimal <75414938+itsaimal@users.noreply.github.com>
2021-01-27 14:58:55 +0500

    Update Cover2.js

--- src/About/Cover2.js
+++ src/About/Cover2.js
@@ -8,7 +8,7 @@ function Cover2() {
 
             <div className="flex justify-center">
             <div className="mr-16">
-<img className="mb-8" src="https://fleming.app/img/landing/png/team/member-domas-rounded.png"/>
+<img className="mb-8" src="https://fleming.app/img/landing/png/team/member-domas-rounded.png" alt="pic"/>
 
 <h1 className="w-44 text-blue-800 text-lg font-semibold ml-6 mb-2 cursor-pointer hover:text-gray-700 text-center"><LinkedInIcon className="mr-1"/>Domas Pleškus
 
@@ -16,7 +16,7 @@ function Cover2() {
 <h1 className="text-center">CEO</h1>
             </div>
             <div>
-                <img className="mb-8" src="https://fleming.app/img/landing/png/team/member-mantas-rounded.png"/>
+                <img className="mb-8" src="https://fleming.app/img/landing/png/team/member-mantas-rounded.png" alt="pic"/>
 
                 <h1 className="w-48 text-blue-800 text-lg font-semibold  mb-2 ml-8 cursor-pointer hover:text-gray-700 text-center"><LinkedInIcon className="mr-1"/>Mantas Bacevičius</h1>
                 <h1 className="text-center">COO</h1>
